4434cdb9b06df588227a404936b225b924d10d9c
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-25 13:56:54 +0900

    articles Model add

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,9 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    edite_at = models.DateTimeField(auto_now=True)
