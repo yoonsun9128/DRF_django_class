c5738fd3ae48e889d097c36ad2b1e96ebf728874
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-25 23:12:16 +0900

    swagger and class type use

--- articles/urls.py
+++ articles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from articles import views
 
 urlpatterns = [
-    path('',views.articleAPI, name="index"),
+    path('',views.ArticleList.as_view(), name="index"),
     path('<int:article_id>/', views.article_detail, name="detail"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -6,17 +6,34 @@ from rest_framework.generics import get_object_or_404
 from articles.models import Articles
 from articles.serializers import ArticlesSerializers
 
-# Create your views here.
-@api_view(['GET','POST'])
-def articleAPI(request):
-    if request.method == 'GET':
+# @api_view(['GET','POST'])
+# def articleAPI(request):
+#     if request.method == 'GET':
+#         articles = Articles.objects.all()
+#         # article = articles[0]
+#         # serializer = ArticlesSerializers(articles)
+#         #장고의 쿼리 형테를 딕션너리 형태로 변경하는중
+#         serializer = ArticlesSerializers(articles, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         # print(request.data)
+#         serializer = ArticlesSerializers(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_201_CREATED)
+#         else:
+#             print(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         return Response()
+
+class ArticleList(APIView):
+    def get(self, request, format=None):
         articles = Articles.objects.all()
         # article = articles[0]
         # serializer = ArticlesSerializers(articles)
         #장고의 쿼리 형테를 딕션너리 형태로 변경하는중
         serializer = ArticlesSerializers(articles, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         # print(request.data)
         serializer = ArticlesSerializers(data=request.data)
         if serializer.is_valid():
@@ -40,6 +57,26 @@ def article_detail(request, article_id):
             serializer.save()
             return Response(serializer.data)
     elif request.method == 'DELETE':
+        article = get_object_or_404(Articles, id=article_id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ArticleDetail(APIView):
+    def get(self, request, article_id, format=None):
+        # article = Articles.objects.get(id=article_id)
+        article = get_object_or_404(Articles, id=article_id)
+        serializer = ArticlesSerializers(article)
+        return Response(serializer.data)
+
+    def put(self, request, article_id, format=None):
+        article = get_object_or_404(Articles, id=article_id)
+        serializer = ArticlesSerializers(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, article_id, format=None):
         article = get_object_or_404(Articles, id=article_id)
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
