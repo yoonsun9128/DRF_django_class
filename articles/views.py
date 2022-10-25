88ed29814ae7b7cddc7f2a6592cd66df80e36385
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-25 16:59:58 +0900

    method=post add

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from articles.models import Articles
 from articles.serializers import ArticlesSerializers
@@ -6,8 +7,19 @@ from articles.serializers import ArticlesSerializers
 # Create your views here.
 @api_view(['GET','POST'])
 def index(request):
-    articles = Articles.objects.all()
-    # article = articles[0]
-    # serializer = ArticlesSerializers(articles)
-    serializer = ArticlesSerializers(articles, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        articles = Articles.objects.all()
+        # article = articles[0]
+        # serializer = ArticlesSerializers(articles)
+        #장고의 쿼리 형테를 딕션너리 형태로 변경하는중
+        serializer = ArticlesSerializers(articles, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        # print(request.data)
+        serializer = ArticlesSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            print(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response()
